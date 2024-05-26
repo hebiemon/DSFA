a4da3b8dae8b9b32d3e46bf64430d79e1fae3607
圏内 <souichiroup@protonmail.com>
2024-05-26 22:41:02 +0900

    astro update & build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
-import { defineConfig } from "astro/config"
-import mdx from "@astrojs/mdx"
-import sitemap from "@astrojs/sitemap"
-import tailwind from "@astrojs/tailwind"
-import solidJs from "@astrojs/solid-js"
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-sphere-demo.vercel.app",
-  integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
-})
+  integrations: [mdx(), sitemap(), solidJs(), tailwind({
+    applyBaseStyles: false
+  })],
+  output: "server",
+  adapter: netlify()
+});
